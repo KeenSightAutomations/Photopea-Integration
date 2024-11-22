eb1783e971b53ef44f7fd8e60cfe3cabc9a05324
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-11-22 22:37:41 +0200

    add version to automate photopea using command line

--- photopea-cli/README.md
+++ photopea-cli/README.md
@@ -1,9 +1,28 @@
-## Photopea using CLI
+# Photopea using CLI
 
-# Installation and Setup
-    cd cd Photopea-Integration/photopea-cli
-    npm install
+# Prerequisites
+* Python 3: Ensure Python 3.x is installed on your machine.
+* Local HTTP Server: The script assumes you are running a local HTTP server (using Python's built-in http.server module) to serve your images and PSD files. Make sure to run it in the same directory that contains the PSD file and the input directory of images.
+* Web Browser: The script uses the default web browser to open Photopea.(I use Google Chrome and it work correctly and not sure if it work to other browsers or not so make sure google chrome is default browser)
+* install this extension on default browser and make it ON (to resolve problem related with cross origin):
+    https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf
 
-    Usage: node index.js <psdFilePath> <inputFolder> <outputFolder>
-    Ex: node script.js smartObject.psd input out
-    
+
+# How To run
+
+## Run a Local HTTP Server
+* In the terminal, navigate to the folder where your images and PSD file are located.
+* Start a local HTTP server to serve the files using the following command:
+    ```
+    cd Photopea-Integration/photopea-cli
+    python3 -m http.server 8000
+    ```
+
+## Execute the Script
+* Run the script from the command line
+    ```
+    Usage: python3 script.py <psd_file_path> <input_directory>
+    Ex: python3 script.py smartObject.psd input
+    ```
+* Replace <psd_file_path> with the path to your PSD file (e.g., smartObject.psd).
+* Replace <input_directory> with the path to the folder containing the images you want to insert into the PSD file (e.g., input)
---
+++ photopea-cli/script.py
@@ -0,0 +1,127 @@
+import os
+import urllib.parse
+import webbrowser
+import json
+import time
+import sys
+
+
+# Function to open PSD file in Photopea
+def send_script_to_photopea(imagePath, psd_file_path, server_url, script):
+    # Prepare the JSON configuration
+    config = {
+        "files": [f"{server_url}/{imagePath}",f"{server_url}/{psd_file_path}"],
+        "server": f"{server_url}/save_image.php", 
+        "script": script
+    }
+
+    # Convert the config to JSON and encode it as a URL parameter
+    json_config = json.dumps(config)
+    encoded_config = urllib.parse.quote(json_config)
+
+    # Construct the full URL to open in the browser
+    url = f"https://www.photopea.com#{encoded_config}"
+
+    # Open the URL in the browser
+    webbrowser.open(url)
+
+
+# Function to process images in a folder
+def process_images_in_folder(imagePath, psd_file_path, server_url):
+    """Process all images in the folder."""
+    # Open the PSD file in Photopea
+    timestamp = int(time.time() * 1000)
+    commands = [
+        '''
+        var smartObjectLayer = null;
+        var psdDoc = app.documents[1];
+        app.aactiveDocument = psdDoc
+        var lays = app.activeDocument.layers;
+        for(var i=0; i< lays.length; i++)
+        {
+            smartObjectLayer = lays[i]
+            if (smartObjectLayer.kind === LayerKind.SMARTOBJECT){
+                alert("smartObjectLayer: " + smartObjectLayer.name + ' Num ' + i)
+                break;
+            }
+        }
+        if (!smartObjectLayer || smartObjectLayer.kind != LayerKind.SMARTOBJECT) {
+                alert("Image Opened");
+            }
+        else {
+        ''',
+            'app.activeDocument.activeLayer = app.activeDocument.layers[i];'
+            'var idEditContents = stringIDToTypeID("placedLayerEditContents");',
+            'var desc = new ActionDescriptor();',
+            'executeAction(idEditContents, desc, DialogModes.NO);',
+            '''
+            var imageDoc = app.documents[0];
+            var smartLayer = app.documents[2];
+            app.activeDocument = imageDoc;
+            imageDoc.activeLayer.copy(); 
+            app.activeDocument = smartLayer;
+            smartLayer.paste();
+
+            var soDoc = app.activeDocument;
+            var placedLayer = soDoc.activeLayer;  
+            var soWidth = soDoc.width;
+            var soHeight = soDoc.height;
+            var imgWidth = placedLayer.bounds[2] - placedLayer.bounds[0];
+            var imgHeight = placedLayer.bounds[3] - placedLayer.bounds[1];
+            
+            var soRatio = soWidth / soHeight;
+            var imgRatio = imgWidth / imgHeight;
+            var scalePercentage;
+
+            if (imgRatio > soRatio) {
+                scalePercentage = (soHeight / imgHeight) * 100;
+            } else {
+                scalePercentage = (soWidth / imgWidth) * 100;
+            }
+            placedLayer.resize(scalePercentage, scalePercentage, AnchorPosition.MIDDLECENTER);
+            
+            // Clean up extra layers if necessary
+            var nowDoc = app.activeDocument;
+            while (nowDoc.layers.length > 1) {
+                nowDoc.layers[1].remove();  // Remove the original layer if it's not the smart object layer
+            }
+            
+            app.activeDocument.save();
+            app.activeDocument.close();
+            app.activeDocument = app.documents[1];
+            '''
+            
+
+            'var exportOptions = new ExportOptionsSaveForWeb();',
+            'exportOptions.format = SaveDocumentType.JPEG;',
+            'exportOptions.quality = 100;',
+            f'var outputName = "export_{timestamp}.jpg";',
+            'app.activeDocument.exportDocument(new File(outputName), ExportType.SAVEFORWEB, exportOptions);',
+        '}'
+    ]
+
+    send_script_to_photopea(imagePath, psd_file_path, server_url, ";".join(commands))
+    time.sleep(5)
+
+
+
+# Main function
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 script.py <psd_file> <input_directory>")
+        sys.exit(1)
+
+    psd_file_path = sys.argv[1]  # Path to your PSD file
+    input_dir = sys.argv[2]  # Folder containing images to insert
+    port = 8000
+    server_url = f"http://localhost:{port}"
+
+    files = os.listdir(input_dir)
+
+    for file in files:
+        file_path = input_dir + '/' + file
+        print(file_path)
+    # Process the PSD and images
+        process_images_in_folder(file_path, psd_file_path, server_url)
+
+
